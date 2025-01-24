3745f83f8036f465f27290c6c3a792dad9153cc7
tk5g23 <tk5g23@soton.ac.uk>
2025-01-24 21:57:35 +0000

    Initial Commit

--- codebase/Network.java
+++ codebase/Network.java
@@ -1,6 +1,26 @@
 package codebase;
 
+import java.util.List;
+import java.util.Map;
+
 public class Network {
+
+  // Represents an edge (connection) between two stations, the line connecting them and the travel time
+  static class Edge {
+    String destination;
+    String line;
+    int travelTime;
+
+    Edge(String nDestination, String nLine, int nTravelTime) {
+      destination = nDestination;
+      line = nLine;
+      travelTime = nTravelTime;
+    }
+  }
+
+  private final Map<String, List<Edge>> tflNetwork;
+
+
 
 
 
