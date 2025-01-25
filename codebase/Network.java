169ec8da211d53c18c24eca875986491e32a23d8
tk5g23 <tk5g23@soton.ac.uk>
2025-01-25 15:18:49 +0000

    Getter Functions added to Class Network

--- codebase/Network.java
+++ codebase/Network.java
@@ -9,40 +9,44 @@ public class Network {
 
   // Represents an edge (connection) between two stations, the line connecting them and the travel time
   static class Edge {
-    String destination;
+    Station destination;
     String line;
     int travelTime;
 
-    Edge(String nDestination, String nLine, int nTravelTime) {
-      destination = nDestination;
-      line = nLine;
-      travelTime = nTravelTime;
+    Edge(Station nDestination, String nLine, int nTravelTime) {
+      this.destination = nDestination;
+      this.line = nLine;
+      this.travelTime = nTravelTime;
     }
   }
 
-  public static Map<String, List<Edge>> tflNetwork;
+  private final Map<Station, List<Edge>> tflNetwork;
 
   // initialises the tfl network graph
   public Network () {
-    tflNetwork = new HashMap<>();
+    this.tflNetwork = new HashMap<>();
   }
 
   // Add a new node (ie station) to the tfl network
-  public void addStation(String stationName){
-    tflNetwork.put(stationName, new ArrayList<>());
+  public void addStation(Station stationName){
+    tflNetwork.putIfAbsent(stationName, new ArrayList<>());
   }
 
   // Adds a connection between two stations
-  public void addEdge(String src, String dest, String line, int travelTime) {
-    tflNetwork.put(src, new ArrayList<>());
-    tflNetwork.put(dest, new ArrayList<>());
+  public void addEdge(Station src, Station dest, String line, int travelTime) {
+    tflNetwork.putIfAbsent(src, new ArrayList<>());
+    tflNetwork.putIfAbsent(dest, new ArrayList<>());
     tflNetwork.get(src).add(new Edge(dest, line, travelTime));
     tflNetwork.get(dest).add(new Edge(src, line, travelTime));
   }
 
-  public void removeEdge(String src, String dest) {
-    tflNetwork.get(src).remove(dest);
-    tflNetwork.get(dest).remove(src);
+  public List<Edge> getStation(Station station) {
+    List<Edge> connection = tflNetwork.get(station);
+    return connection;
+  }
+
+  public Map<Station, List<Edge>> getTflNetwork(){
+    return this.tflNetwork;
   }
 
 }
